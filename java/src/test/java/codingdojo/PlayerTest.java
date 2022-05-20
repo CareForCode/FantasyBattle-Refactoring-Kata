6bf99dbc950a066ec451be5916a79863252c3fe6
Patrick Scheibert <patrick.scheibert@hotmail.com>
2022-05-20 10:51:54 +0200

    reduce test duplication

--- java/src/test/java/codingdojo/PlayerTest.java
+++ java/src/test/java/codingdojo/PlayerTest.java
@@ -9,32 +9,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 
 public class PlayerTest {
+
+    private final Inventory inventory = new Inventory(new Equipment(
+            new BasicItem("lefthand", 1, 0.8f),
+            new BasicItem("rightHand", 2, 0.5f),
+            new BasicItem("head", 3, 0.3f),
+            new BasicItem("feet", 5, 0.2f),
+            new BasicItem("chest", 8, 0.1f)));
+    private final Stats stats = new Stats(13);
+    private final Player player = new Player(inventory, stats);
+    private final SimpleArmor armor = new SimpleArmor(1);
+
     @Test
     void damageEnemyWithoutBuffs() {
-        Inventory inventory = new Inventory(new Equipment(
-                new BasicItem("lefthand", 1, 0.8f),
-                new BasicItem("rightHand", 2, 0.5f),
-                new BasicItem("head", 3, 0.3f),
-                new BasicItem("feet", 5, 0.2f),
-                new BasicItem("chest", 8, 0.1f)));
-        Stats stats = new Stats(13);
-        SimpleEnemy target = new SimpleEnemy(new SimpleArmor(1), Collections.emptyList());
-        Damage damage = new Player(inventory, stats).calculateDamage(target);
+        SimpleEnemy target = new SimpleEnemy(armor, Collections.emptyList());
+
+        Damage damage = player.calculateDamage(target);
+
         assertEquals(60, damage.getAmount());
     }
 
-
     @Test
     void damageEnemyWithBuff() {
-        Inventory inventory = new Inventory(new Equipment(
-                new BasicItem("lefthand", 1, 0.8f),
-                new BasicItem("rightHand", 2, 0.5f),
-                new BasicItem("head", 3, 0.3f),
-                new BasicItem("feet", 5, 0.2f),
-                new BasicItem("chest", 8, 0.1f)));
-        Stats stats = new Stats(13);
-        SimpleEnemy target = new SimpleEnemy(new SimpleArmor(1), Collections.singletonList(new BasicBuff(3f, 5f)));
-        Damage damage = new Player(inventory, stats).calculateDamage(target);
+        SimpleEnemy target = new SimpleEnemy(armor, Collections.singletonList(new BasicBuff(3f, 5f)));
+
+        Damage damage = player.calculateDamage(target);
+
         assertEquals(57, damage.getAmount());
     }
 }
