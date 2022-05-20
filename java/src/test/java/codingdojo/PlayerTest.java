550a09860918d66b185f0299188b7546d32f70bf
Patrick Scheibert <patrick.scheibert@hotmail.com>
2022-05-20 10:26:55 +0200

    2nd test with different values

--- java/src/test/java/codingdojo/PlayerTest.java
+++ java/src/test/java/codingdojo/PlayerTest.java
@@ -24,4 +24,18 @@ public class PlayerTest {
         assertEquals(0, damage.getAmount());
     }
 
+    @Test
+    void damageCalculations2() {
+        Inventory inventory = new Inventory(new Equipment(
+                new BasicItem("lefthand", 10, 1.0f),
+                new BasicItem("rightHand", 10, 1.0f),
+                new BasicItem("head", 10, 1.0f),
+                new BasicItem("feet", 10, 1.0f),
+                new BasicItem("chest", 10, 1.0f)));
+        Stats stats = new Stats(0);
+        SimpleEnemy target = new SimpleEnemy(new SimpleArmor(0), Collections.emptyList());
+        Damage damage = new Player(inventory, stats).calculateDamage(target);
+        assertEquals(250, damage.getAmount());
+    }
+
 }
