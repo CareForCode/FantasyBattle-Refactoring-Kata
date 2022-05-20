52c080835b067717c77e97e4d7e3e718109bbe4c
rgerm <roland.germ@admiral.at>
2022-05-20 10:14:16 +0200

    test: implement 1st test with dummy values

--- java/src/test/java/codingdojo/PlayerTest.java
+++ java/src/test/java/codingdojo/PlayerTest.java
@@ -3,6 +3,8 @@ package codingdojo;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 
@@ -10,11 +12,16 @@ public class PlayerTest {
 
     @Test
     void damageCalculations() {
-        Inventory inventory = new Inventory(null);
+        Inventory inventory = new Inventory(new Equipment(
+                new BasicItem("", 0, 0.0f),
+                new BasicItem("", 0, 0.0f),
+                new BasicItem("", 0, 0.0f),
+                new BasicItem("", 0, 0.0f),
+                new BasicItem("", 0, 0.0f)));
         Stats stats = new Stats(0);
-        SimpleEnemy target = new SimpleEnemy(null, null);
+        SimpleEnemy target = new SimpleEnemy(new SimpleArmor(1), Collections.emptyList());
         Damage damage = new Player(inventory, stats).calculateDamage(target);
-        assertEquals(10, damage.getAmount());
+        assertEquals(0, damage.getAmount());
     }
 
 }
