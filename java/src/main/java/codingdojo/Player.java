5041ef6ab5f01be58d858aadabf8225226de481e
Patrick Scheibert <patrick.scheibert@outlook.com>
2022-08-12 10:54:19 +0200

    move soak method to target

--- java/src/main/java/codingdojo/Player.java
+++ java/src/main/java/codingdojo/Player.java
@@ -12,21 +12,8 @@ class Player extends Target {
 
     Damage calculateDamage(Target other) {
         int totalDamage = equipment.getTotalDamage(stats.getStrength());
-        int soak = getSoak(other, totalDamage);
+        int soak = other.getSoak(totalDamage);
         return new Damage(Math.max(0, totalDamage - soak));
-    }
-
-    private int getSoak(Target other, int totalDamage) {
-        int soak = 0;
-        if (other instanceof Player) {
-            // TODO: Not implemented yet
-            //  Add friendly fire
-            soak = totalDamage;
-        } else if (other instanceof SimpleEnemy) {
-            SimpleEnemy simpleEnemy = (SimpleEnemy) other;
-            soak = simpleEnemy.getSoak();
-        }
-        return soak;
     }
 
 }
--- java/src/main/java/codingdojo/Target.java
+++ java/src/main/java/codingdojo/Target.java
@@ -1,4 +1,16 @@
 package codingdojo;
 
 abstract class Target {
+    int getSoak(int totalDamage) {
+        int soak = 0;
+        if (this instanceof Player) {
+            // TODO: Not implemented yet
+            //  Add friendly fire
+            soak = totalDamage;
+        } else if (this instanceof SimpleEnemy) {
+            SimpleEnemy simpleEnemy = (SimpleEnemy) this;
+            soak = simpleEnemy.getSoak();
+        }
+        return soak;
+    }
 }
