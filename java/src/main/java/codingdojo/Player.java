66a0b623d4a10c0d148dfba07417fc7b0aca0e7e
Patrick Scheibert <patrick.scheibert@outlook.com>
2022-08-12 10:30:59 +0200

    move code to calculate equipment damage from player to equipment

--- java/src/main/java/codingdojo/Player.java
+++ java/src/main/java/codingdojo/Player.java
@@ -11,8 +11,8 @@ class Player extends Target {
     }
 
     Damage calculateDamage(Target other) {
-        int baseDamage = getBaseDamage();
-        float damageModifier = getDamageModifier();
+        int baseDamage = inventory.getEquipment().getBaseDamage();
+        float damageModifier = inventory.getEquipment().getDamageModifier(stats.getStrength());
         int totalDamage = Math.round(baseDamage * damageModifier);
         int soak = getSoak(other, totalDamage);
         return new Damage(Math.max(0, totalDamage - soak));
@@ -40,33 +40,4 @@ class Player extends Target {
         return soak;
     }
 
-    private float getDamageModifier() {
-        Equipment equipment = this.inventory.getEquipment();
-        Item leftHand = equipment.getLeftHand();
-        Item rightHand = equipment.getRightHand();
-        Item head = equipment.getHead();
-        Item feet = equipment.getFeet();
-        Item chest = equipment.getChest();
-        float strengthModifier = stats.getStrength() * 0.1f;
-        return strengthModifier +
-            leftHand.getDamageModifier() +
-            rightHand.getDamageModifier() +
-            head.getDamageModifier() +
-            feet.getDamageModifier() +
-            chest.getDamageModifier();
-    }
-
-    private int getBaseDamage() {
-        Equipment equipment = this.inventory.getEquipment();
-        Item leftHand = equipment.getLeftHand();
-        Item rightHand = equipment.getRightHand();
-        Item head = equipment.getHead();
-        Item feet = equipment.getFeet();
-        Item chest = equipment.getChest();
-        return leftHand.getBaseDamage() +
-        rightHand.getBaseDamage() +
-        head.getBaseDamage() +
-        feet.getBaseDamage() +
-        chest.getBaseDamage();
-    }
 }
